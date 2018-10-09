8d8a9f0b105acdbeb3d3df5a908caf18ea2f1b64
Carlos Toxtli <ctoxtli@gmail.com>
2018-10-08 23:22:51 -0400

    Files changed

--- build/1.js
+++ build/1.js
@@ -186,7 +186,7 @@ var Config = exports.Config = {
   interpolationCount: 4,
   title: 'NSynth: Sound Maker',
   subtitle: 'Make unusual new sounds with machine learning.',
-  audioFolder: './sounds',
+  audioFolder: 'http://www.carlostoxtli.com/NSynth-Compiled/sounds',
   teal: teal,
   purple: purple
 };
